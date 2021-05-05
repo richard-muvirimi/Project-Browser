0051791e86cb5fb61768d74b1a67c43c88014f65
Richard Muvirimi <tygalive@gmail.com>
2021-05-05 21:21:36 +0200

    fix pagination over calculation

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://192.168.43.172/projects/';
+	public $baseURL = '';
 
 	/**
 	 * --------------------------------------------------------------------------
--- app/Views/partial/pagination.php
+++ app/Views/partial/pagination.php
@@ -1,18 +1,15 @@
 <div class="w3-center w3-padding-32">
     <div class="w3-bar">
 
-        <a href="<?= current_url(true)->setQuery("?page=" . ($page - 1)) ?>"
-            class="w3-bar-item w3-button w3-hover-teal <?= $page == 1 ? "w3-disabled" : "" ?>">«</a>
+        <a href="<?= current_url(true)->setQuery("?page=" . ($page - 1)) ?>" class="w3-bar-item w3-button w3-hover-teal <?= $page == 1 ? "w3-disabled" : "" ?>">«</a>
 
         <?php $chunks = floor($total / $chunk) ?>
 
-        <?php for ($i = 0; $i <=  $chunks; $i++) : ?>
+        <?php for ($i = 0; $i <  $chunks; $i++) : ?>
 
-        <a href="<?= $page == $i + 1 ? "#" : current_url(true)->setQuery("?page=" . ($i + 1)) ?>"
-            class="w3-bar-item w3-button w3-hover-teal <?= $page == ($i + 1) ? "w3-teal" : "" ?>"><?= $i + 1 ?></a>
+            <a href="<?= $page == $i + 1 ? "#" : current_url(true)->setQuery("?page=" . ($i + 1)) ?>" class="w3-bar-item w3-button w3-hover-teal <?= $page == ($i + 1) ? "w3-teal" : "" ?>"><?= $i + 1 ?></a>
 
         <?php endfor; ?>
-        <a href="<?= current_url(true)->setQuery("?page=" .  ($page + 1)) ?>"
-            class="w3-bar-item w3-button w3-hover-teal <?= $page ==  ($chunks + 1) ? "w3-disabled" : "" ?>">»</a>
+        <a href="<?= current_url(true)->setQuery("?page=" .  ($page + 1)) ?>" class="w3-bar-item w3-button w3-hover-teal <?= $page ==  ($chunks + 1) ? "w3-disabled" : "" ?>">»</a>
     </div>
 </div>
